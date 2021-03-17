bfec2c9543c80e486a8e3f61c2ea730b2913d792
Jose Fonseca <joselfonseca97@gmail.com>
2021-03-16 23:14:51 -0600

    Updates

---
+++ precache-manifest.a50287784580f1880dd1af3457f2364f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa6923ceb01291087b1c29d8984c5d1",
+    "url": "/conecta4/index.html"
+  },
+  {
+    "revision": "3023b390edee960f4b74",
+    "url": "/conecta4/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "b87063dc5585b03b810d",
+    "url": "/conecta4/static/css/main.3251b952.chunk.css"
+  },
+  {
+    "revision": "3023b390edee960f4b74",
+    "url": "/conecta4/static/js/2.6baede71.chunk.js"
+  },
+  {
+    "revision": "b50f7ffc22e7e83c443c86e0676e4851",
+    "url": "/conecta4/static/js/2.6baede71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b87063dc5585b03b810d",
+    "url": "/conecta4/static/js/main.d61f5806.chunk.js"
+  },
+  {
+    "revision": "95982bee854b824cea8a",
+    "url": "/conecta4/static/js/runtime-main.1b887572.js"
+  },
+  {
+    "revision": "db3015a57e0b8bbce1d82a41c7ccb604",
+    "url": "/conecta4/static/media/Ingame-Background.db3015a5.jpg"
+  },
+  {
+    "revision": "08f563c2b24bfde4e731b8aad4ccdc52",
+    "url": "/conecta4/static/media/main_background.08f563c2.jpg"
+  }
+]);
